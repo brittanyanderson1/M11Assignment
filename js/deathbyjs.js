26825f2c49b754ad62ff0119d5c546a07b7aaee0
Brittany Anderson <142555405+brittanyanderson1@users.noreply.github.com>
2025-01-16 15:53:37 -0800

    deathbyjs.js tested and complete

--- js/deathbyjs.js
+++ js/deathbyjs.js
@@ -1,9 +1,20 @@
 //STEP 1
+//let aToZ = str => str.split('').sort().join('') 
 
+//console.log(aToZ('webmaster'))
 //STEP 2
+//let capFirstLetter = str => str.split(' ').map(word => word[0].toUpperCase() + word.slice(1)).join(' ') 
 
+//console.log(capFirstLetter('the quick brown fox'))
 //STEP 3
+//let numVowels = str => (str.match(/[aeiou]/gi) || []).length 
 
+//console.log(numVowels('The quick brown fox'))
 //STEP 4
+//let ranGenString = length => [...Array(length)].map(() => 'ABCDEFGHstuvwxyz01234567'.charAt(Math.random() * 75)).join('') 
 
+//console.log(ranGenString(8))
 //STEP 5
+//let longestCountryName = countries => countries.reduce((longest, country) => country.length > longest.length ? country : longest, '') 
+
+//console.log(longestCountryName(['Greece', 'Sweden', 'New Zealand', 'Argentina', 'United Kingdom']))
