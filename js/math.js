30295450ccd5e6fbb9966af323ceeb23e8291fa7
Brittany Anderson <142555405+brittanyanderson1@users.noreply.github.com>
2025-01-15 17:57:57 -0800

    math.js tested and complete

--- js/math.js
+++ js/math.js
@@ -1,9 +1,43 @@
 //STEP 1
+//let numVal = prompt(' Enter any number below, negative or positive.')
+
+//let posNumVal = Math.abs(numVal)
+
+//console.log(`The absolute value of ${numVal} is ${posNumVal}`)
 
 //STEP 2
+//let floatNum = prompt('Enter any decimal number below.')
+
+//let roundNum = Math.ceil(floatNum)
+
+//console.log(`${floatNum} rounded up is ${roundNum}`)
 
 //STEP 3
+//let floatNum = prompt('Enter any decimal number below.')
+
+//let roundNum = Math.floor(floatNum)
+
+//console.log(`${floatNum} rounded down is ${roundNum}`)
 
 //STEP 4
+//let nums = prompt('Enter 5 numbers seperated by commas below. ex. 1,2,3,4,5')
+
+//let numList = nums.split(',').map(Number)
+
+//let highestNum = Math.max(...numList)
+
+//let lowwestNum = Math.min(...numList)
+
+//console.log(`The smallet number is ${lowwestNum} and the largest number is ${highestNum}`)
+
+
 
 //STEP 5
+
+//let userNum = prompt('Enter any number below.')
+
+//let numVal = parseFloat(userNum)
+
+//let sqRtVal = Math.sqrt(numVal)
+
+//console.log(`The square root of ${numVal} is ${sqRtVal}`)
